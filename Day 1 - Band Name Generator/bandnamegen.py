52ea73e780aa299ec7dd6003c08729d753b0a760
Vedant Singh Chauhan <121000404+vedantttts@users.noreply.github.com>
2023-07-28 20:29:22 +0530

    Add files via upload

---
+++ Day 1 - Band Name Generator/bandnamegen.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+name = input("What is your city name?\n")
+pet = input("What is your pet's name?\n")
+print("Your Band name can be : " + name + " " + pet)
