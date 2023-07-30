1245b246e57bc702efabe4a0f396ca11c78b8d92
Vedant Singh Chauhan <121000404+vedantttts@users.noreply.github.com>
2023-07-30 22:20:49 +0530

    Add files via upload

---
+++ Day 8 - Caesar Cypher/cs.py
@@ -0,0 +1,49 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
+
+
+def caeser (direction,text,shift):
+    s = ""
+    if direction == "decrypt":
+        shift *=-1
+    for char in text:
+        if char in alphabets:
+            s+= alphabets[(alphabets.index(char) + shift)%26]
+        else :
+            s+=char    
+        
+    print(f"The {direction}ed code is [{s}] ")
+    
+    
+    
+should_end = False
+while not should_end:
+    direction = input("What do you want to do? \"Encrypt\" or \"Decrypt\" : ").lower()
+    text = input("Enter Your Text : ").lower()
+    shift = int(input("Enter the amount of shift : "))
+    caeser (direction,text,shift)
+
+    lassan = input("Do you want to continue? \"yes\" or \"no\" : ")
+    if lassan == "no":
+        should_continue = True 
+        print("Good Bye")
+
+
